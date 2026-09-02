3150107b7e9c4c259f2066051b41715df1ddbb62
godixlearn <godix.learn@gmail.com>
2026-09-02 20:22:11 +0530

    Add Day2 image to the project

---
+++ JS/Day1/script.js
@@ -0,0 +1,17 @@
+
+
+
+document.getElementById("btn").onclick = function(){
+
+    document.getElementById("btn").textContent = "You clicked me!";
+
+    document.getElementById("btn").style.backgroundColor = "Green";
+
+    document.getElementById("btn").style.color = "White";
+
+
+    document.getElementById("message").textContent = "Javascript is Working!";
+
+    alert("You clicked the button!");
+
+}
