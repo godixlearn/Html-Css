c440881a3c588f8946d1a282b39f7c6ce72ccd69
godixlearn <godix.learn@gmail.com>
2026-09-14 18:33:34 +0530

    Add filter functionality for product search and categorization

---
+++ JS/Day7/filter.js
@@ -0,0 +1,39 @@
+// const products = [
+//   { name: "Apple", price: 120, category: "fruits" },
+//   { name: "Banana", price: 40, category: "fruits" },
+//   { name: "Broccoli", price: 80, category: "vegetables" },
+//   { name: "Milk", price: 60, category: "dairy" }
+// ];
+
+// const productName = "Apple";
+
+// const searchedProducts = products.filter((product)=>{
+//     return products.name === productName;
+// });
+
+// // console.log(searchedProducts);
+
+// const affordableProducts = products.filter((product)=>{
+//     // console.log(product.price < 100);
+//     return product.price < 100;
+// })
+
+// // console.log(affordableProducts);
+
+
+// const friutCategory = products.filter((product)=>{
+//     return product.category === "fruits";
+// }) 
+
+// // console.log(friutCategory);
+
+
+// const searchText = 'A';
+
+// const searchResult = products.filter((product)=>{
+//     return product.name.toLowerCase().includes(searchText.toLowerCase())
+// })
+
+// console.log(searchResult);
+
+
