2ddf873f21b4861d880ad412698a3f87fcd93300
godixlearn <godix.learn@gmail.com>
2026-09-14 18:33:42 +0530

    Uncomment product filtering logic and improve readability in filter.js

--- JS/Day7/filter.js
+++ JS/Day7/filter.js
@@ -1,39 +1,39 @@
-// const products = [
-//   { name: "Apple", price: 120, category: "fruits" },
-//   { name: "Banana", price: 40, category: "fruits" },
-//   { name: "Broccoli", price: 80, category: "vegetables" },
-//   { name: "Milk", price: 60, category: "dairy" }
-// ];
+const products = [
+  { name: "Apple", price: 120, category: "fruits" },
+  { name: "Banana", price: 40, category: "fruits" },
+  { name: "Broccoli", price: 80, category: "vegetables" },
+  { name: "Milk", price: 60, category: "dairy" }
+];
 
-// const productName = "Apple";
+const productName = "Apple";
 
-// const searchedProducts = products.filter((product)=>{
-//     return products.name === productName;
-// });
+const searchedProducts = products.filter((product)=>{
+    return products.name === productName;
+});
 
-// // console.log(searchedProducts);
+// console.log(searchedProducts);
 
-// const affordableProducts = products.filter((product)=>{
-//     // console.log(product.price < 100);
-//     return product.price < 100;
-// })
+const affordableProducts = products.filter((product)=>{
+    // console.log(product.price < 100);
+    return product.price < 100;
+})
 
-// // console.log(affordableProducts);
-
-
-// const friutCategory = products.filter((product)=>{
-//     return product.category === "fruits";
-// }) 
-
-// // console.log(friutCategory);
+// console.log(affordableProducts);
 
 
-// const searchText = 'A';
+const friutCategory = products.filter((product)=>{
+    return product.category === "fruits";
+}) 
 
-// const searchResult = products.filter((product)=>{
-//     return product.name.toLowerCase().includes(searchText.toLowerCase())
-// })
+// console.log(friutCategory);
 
-// console.log(searchResult);
+
+const searchText = 'A';
+
+const searchResult = products.filter((product)=>{
+    return product.name.toLowerCase().includes(searchText.toLowerCase())
+})
+
+console.log(searchResult);
 
 
